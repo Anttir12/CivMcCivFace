c2931489b8b52d20921ac69e616349b0af3747f2
Antti <antti.ruohisto@gmail.com>
2020-06-21 23:01:47 +0300

    massive restructuing

---
+++ civ_mc_civ_face/cogs.py
@@ -0,0 +1,63 @@
+import json
+
+from discord.ext import commands
+
+from civ_mc_civ_face.mc_civ_brains import McCivBrains, McCivBrainException
+
+
+class Game(commands.Cog):
+
+    def __init__(self, brains: McCivBrains):
+        self.bot_brains = brains
+
+    @commands.command()
+    async def create_game(self, ctx, game_name):
+        """ Creates new game to track """
+        try:
+            self.bot_brains.create_game(game_name, ctx.channel.id, ctx.channel.name)
+            await ctx.send("OK, I created a new game called {}".format(game_name))
+        except McCivBrainException as e:
+            await self.send_error_message(ctx, e)
+
+    @commands.command()
+    async def delete_game(self, ctx, game_name):
+        """ Delete game. Bot can no longer track this """
+        try:
+            self.bot_brains.delete_game(game_name)
+            await ctx.send("Ok, I deleted the game \"{}\"".format(game_name))
+        except McCivBrainException as e:
+            await self.send_error_message(ctx, e)
+
+    @commands.command()
+    async def add_player(self, ctx, game_name, ingame_name, discord_name):
+        """ Adds player to the game. This allows bot to add mention to the next turn messages for this user """
+        try:
+            self.bot_brains.add_player_to_game(game_name, ingame_name, discord_name)
+            await ctx.send("{} - {} added to {}".format(ingame_name, discord_name, game_name))
+        except McCivBrainException as e:
+            await self.send_error_message(ctx, e)
+
+    @commands.command()
+    async def remove_player(self, ctx, game_name, ingame_name):
+        """ Remove player from the game. This user no longer gets mentioned by the bot """
+        try:
+            self.bot_brains.remove_player_from_game(game_name, ingame_name)
+            await ctx.send("{} removed from {}".format(ingame_name, game_name))
+        except McCivBrainException as e:
+            await self.send_error_message(ctx, e)
+
+    @commands.command()
+    async def list_games(self, ctx):
+        """ Bot prints entire game db in json format """
+        await ctx.send("Here are currently tracked games:\n{}".format(json.dumps(self.bot_brains.game_db, indent=4,
+                                                                                 sort_keys=True)))
+
+    @commands.command()
+    async def reset(self, ctx):
+        """ Resets the game database. You probably don't want to use this """
+        self.bot_brains.reset()
+        await ctx.send("Game DB has been reset")
+
+
+    async def send_error_message(self, ctx, error):
+        await ctx.send(":poop: ERROR: {} :poop:".format(error))
---
+++ civ_mc_civ_face/mc_civ_brains.py
@@ -0,0 +1,63 @@
+import json
+
+
+class McCivBrainException(Exception):
+    pass
+
+
+class McCivBrains:
+
+    def __init__(self, game_file_location):
+        self.game_file_location = game_file_location
+        with open(self.game_file_location, "r") as file:
+            self.game_db = json.loads(file.read())
+
+    def reset(self):
+        self.game_db = {}
+        self.save_game_database()
+
+    def create_game(self, game_name, channel_id, channel_name):
+        if game_name in self.game_db:
+            raise McCivBrainException("game \"{}\" already exists".format(game_name))
+
+        game_data = {"turn": 0, "players": {}, "channel": {"id": channel_id, "name": channel_name}, "player_turn_order":[""]}
+        self.save_game_data(game_name, game_data)
+
+    def delete_game(self, game_name):
+        if game_name not in self.game_db:
+            raise McCivBrainException("game \"{}\" does not exist".format(game_name))
+        del self.game_db[game_name]
+        self.save_game_database()
+
+    def add_player_to_game(self, game_name, ingame_name, discord_name):
+        game_data = self.game_db.get(game_name)
+        if not game_data:
+            raise McCivBrainException(":poop:ERROR: Could not find game \"{}\"".format(game_name))
+        game_data["players"][ingame_name] = discord_name
+        self.save_game_data(game_name, game_data)
+
+    def remove_player_from_game(self, game_name, ingame_name):
+        game_data = self.game_db.get(game_name)
+        if not game_data:
+            return ":poop:ERROR: Could not find game \"{}\"".format(game_name)
+        if ingame_name not in game_data["players"]:
+            return ":poop:ERROR: Player {} not in game \"{}\"".format(ingame_name, game_name)
+        else:
+            del game_data["players"][ingame_name]
+            self.save_game_database()
+
+    def save_game_database(self):
+        with open(self.game_file_location, "w") as f:
+            f.write(json.dumps(self.game_db))
+
+    def save_game_data(self, game_name: str, data: dict):
+        self.game_db[game_name] = data
+        self.save_game_database()
+
+    def get_discord_username(self, game_name, player_name):
+        game_data = self.game_db.get(game_name)
+        return game_data["players"].get(player_name) if game_data else None
+
+    def get_channel_for_game(self, game_name):
+        game_data = self.game_db.get(game_name)
+        return game_data.get("channel", {}).get("id") if game_data else None
