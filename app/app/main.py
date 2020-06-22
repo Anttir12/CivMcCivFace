4ffd6b701e85581a63cea088b9e78b9ee56f78b9
Antti <antti.ruohisto@gmail.com>
2020-06-22 22:50:50 +0300

    desperate app run uwsgi hack

--- app/app/main.py
+++ app/app/main.py
@@ -10,23 +10,27 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 
-def main():
-    bot = CivMcCivFace(command_prefix="!")
-    http_server = threading.Thread(target=run_server, args=(bot,))
-    discord_bot = threading.Thread(target=run_bot, args=(bot,))
+class App:
 
-    discord_bot.start()
-    http_server.start()
+    def run(self):
+        bot = CivMcCivFace(command_prefix="!")
+        http_server = threading.Thread(target=self.run_server, args=(bot,))
+        discord_bot = threading.Thread(target=self.run_bot, args=(bot,))
 
-
-def run_server(bot):
-    http_server.bot = bot
-    http_server.app.run()
+        discord_bot.start()
+        http_server.start()
 
 
-def run_bot(bot):
-    bot.run(TOKEN)
+    def run_server(self, bot):
+        http_server.bot = bot
+        http_server.app.run()
 
+
+    def run_bot(self, bot):
+        bot.run(TOKEN)
+
+
+app = App()
 
 if __name__ == "__main__":
-    main()
+    app.run()
