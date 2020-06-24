4e3bf1cbdaa6257a3c411b474c0f9398f3ed8555
Antti <antti.ruohisto@gmail.com>
2020-06-24 21:42:23 +0300

    host 0.0.0.0

--- civ_mc_civ_face/http_server.py
+++ civ_mc_civ_face/http_server.py
@@ -13,6 +13,16 @@ async def index(request):
     return response.html('<p>Hello world!</p>')
 
 
+@app.route("/test")
+async def index(request):
+    """ Simply to test if the container can connect to outside world"""
+    import urllib.request
+    page = urllib.request.urlopen("https://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/html/static/index.html")
+    content = page.read()
+    logger.info(content)
+    return response.html(content)
+
+
 @app.route("/play-civ/webhook", methods=["POST"])
 def play_civ_webhook(request):
     data = request.json
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import threading
 import logging.config
@@ -16,11 +17,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = CivMcCivFace(command_prefix="!")
 
 
-def main():
-    discord_bot = threading.Thread(target=bot.run, args=(TOKEN,))
+async def start_bot():
     logger.info("Staring bot with token: {}".format(TOKEN))
-    discord_bot.start()
-    http_server.app.run(port=8888)
+    await bot.start(TOKEN)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
+    try:
+        bot_thread = threading.Thread(target=loop.run_forever)
+        bot_thread.start()
+        # This is a blocking call. Exiting this should continue to stop the bot (loop)
+        http_server.app.run(host='0.0.0.0', port=8888)
+    finally:
+        loop.stop()
 
 
 if __name__ == "__main__":
