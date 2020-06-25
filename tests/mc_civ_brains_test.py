e41e2d82688296da61442bd1061639fcd8df7409
Petri <>
2020-06-25 22:20:39 +0300

    Added None check on early mention player resolving

--- tests/mc_civ_brains_test.py
+++ tests/mc_civ_brains_test.py
@@ -17,5 +17,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, brains.toggle_early_mention("test", "discord_id#1234"))
         self.assertEqual(False, brains.toggle_early_mention("test", "discord_id#1234"))
 
+    def test_add_and_remove_group_notification(self):
+        test_file = "test_game_file.json"
+        brains = McCivBrains(test_file)
+        brains.notify_group("test", "discord_group_id#1234")
+        groups = brains.game_db.get("test")["groups"]
+        self.assertEqual(1, len(groups))
+        brains.remove_group("test", "discord_group_id#1234")
+        self.assertEqual(0, len(groups))
+
 if __name__ == '__main__':
     unittest.main()
